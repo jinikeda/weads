74df8f8d9d55b572eb6a028a5aeedf9ad7c6f112
jinikeda <jikeda@lsu.edu>
2023-09-12 11:16:55 -0500

    speed up the code

--- src/tidaldatums.py
+++ src/tidaldatums.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 # File: tidalDatums.py
-# Name: Peter Bacopoulos
-# Date: August 16, 2021
+# Name: Peter Bacopoulos, Jin Ikeda
+# Date: September 11, 2023
 # Command line: python tidalDatums.py -i harmonics.README -s harmonics.img -d HyControl.img -o tidalDatums.img
 from osgeo import gdal
 from osgeo import osr
 import numpy as np
 import src.basics
 
-#----------------------------------------------------------
+
+# ----------------------------------------------------------
 
 def astronomic_tide_resynthesis(time, omega, phase, amplitude):
     nc = len(amplitude)
     signal = 0
-    for j in range(0,nc):
+    for j in range(0, nc):
         signal += amplitude[j] * np.cos(omega[j] * time - phase[j])
     return signal
+
 
 # Thanks for the help, ChatGPT
 def mean_high_water(data):
@@ -27,14 +29,15 @@ def mean_high_water(data):
     # Find the indices of local maxima in the data
     max_indices = (data[1:-1] > data[:-2]) & (data[1:-1] > data[2:])
     max_indices = np.where(max_indices)[0] + 1
-    
+
     # Extract the values at the local maxima
     max_values = data[max_indices]
-    
+
     # Calculate the mean of the maxima
     MHW = np.mean(max_values)
-    
+
     return MHW
+
 
 # Thanks for the help, ChatGPT
 def mean_low_water(data):
@@ -46,32 +49,31 @@ def mean_low_water(data):
     # Find the indices of local minima in the data
     min_indices = (data[1:-1] < data[:-2]) & (data[1:-1] < data[2:])
     min_indices = np.where(min_indices)[0] + 1
-    
+
     # Extract the values at the local minima
     min_values = data[min_indices]
-    
+
     # Calculate the mean of the minima
     MLW = np.mean(min_values)
-    
+
     return MLW
 
 
-#----------------------------------------------------------
-# F U N C T I O N    T I D A L D A T U M S           
-#----------------------------------------------------------
+# ----------------------------------------------------------
+# F U N C T I O N    T I D A L D A T U M S
+# ----------------------------------------------------------
 #
 # Computes tidal datums of mean low water (MLW), 
 # mean sea level (MSL), and mean high water (MHW)
 # for all nodes hydraulically connected to the ocean.
 # result = function(inputHarmonicsReadMe,inputRasterHarmonics,inputRasterHyControl,outputRasterControl,tstep=900.0)
 # tstep (OPTIONAL, DEFAULT = 900 SECONDS)
-#----------------------------------------------------------
-def tidaldatums(inputHarmonicsReadMe,inputRasterHarmonics,\
-    inputRasterHyControl,outputRasterControl,tstep=900.0):
-
-    #----------------------------------------------------------
+# ----------------------------------------------------------
+def tidaldatums(inputHarmonicsReadMe, inputRasterHarmonics, \
+                inputRasterHyControl, outputRasterControl, tstep=900.0):
+    # ----------------------------------------------------------
     # Check that input files exist
-    #----------------------------------------------------------
+    # ----------------------------------------------------------
     src.basics.fileexists(inputHarmonicsReadMe)
     src.basics.fileexists(inputRasterHarmonics)
     src.basics.fileexists(inputRasterHyControl)
@@ -93,16 +95,16 @@ def tidaldatums(inputHarmonicsReadMe,inputRasterHarmonics,\
     #print ("")
     #print ("Reading rasters")
 
-    htdfin=int(tdfin*2)
+    # htdfin=int(tdfin*2)
     nstep=int((86400*tdfin/tstep)+1)
-    dww=int(24*3600/tstep)
-    hdww=int(dww/2)
+    # dww=int(24*3600/tstep)
+    # hdww=int(dww/2)
     t=tstep*np.ones((nstep,1),dtype=float); t=np.cumsum(t,axis=0);
 
     # --- INITIALIZE ARRAYS ---
-    hdhlwt=np.zeros((hdww,1),dtype=float)
-    hdhlwu=np.zeros((int(htdfin),1),dtype=float)
-    hdhlwd=np.zeros((int(htdfin),1),dtype=float)
+    # hdhlwt=np.zeros((hdww,1),dtype=float)
+    # hdhlwu=np.zeros((int(htdfin),1),dtype=float)
+    # hdhlwd=np.zeros((int(htdfin),1),dtype=float)
 
     harmonics=dict()
     f=open(inputHarmonicsReadMe,'r')
@@ -126,14 +128,10 @@ def tidaldatums(inputHarmonicsReadMe,inputRasterHarmonics,\
     #print ("    DONE!")
 
     rasterHARM=gdal.Open(inputRasterHarmonics)
-    gt=rasterHARM.GetGeoTransform(); pixelSizeX=gt[1]; pixelSizeY=-gt[5];
-
     rasterHC=gdal.Open(inputRasterHyControl)
-    gt=rasterHC.GetGeoTransform()
-    pixelSizeX=gt[1]
-    pixelSizeY=-gt[5];
-         
-    band=rasterHC.GetRasterBand(1); hc=band.ReadAsArray();
+
+    band=rasterHC.GetRasterBand(1);
+    hc=band.ReadAsArray();
     amp=np.zeros((rasterHC.RasterYSize,rasterHC.RasterXSize,nc),dtype=float)
     pha=np.zeros((rasterHC.RasterYSize,rasterHC.RasterXSize,nc),dtype=float)
     for j in range(0,nc):
@@ -145,47 +143,32 @@ def tidaldatums(inputHarmonicsReadMe,inputRasterHarmonics,\
     
     pha=pha*np.pi/180.0
 
-    mlw = np.ones((rasterHC.RasterYSize, rasterHC.RasterXSize), dtype=float) * ndv
-    msl = np.ones((rasterHC.RasterYSize, rasterHC.RasterXSize), dtype=float) * ndv
-    mhw = np.ones((rasterHC.RasterYSize, rasterHC.RasterXSize), dtype=float) * ndv
+    # Stored in valid_indices of water regions (valid_rows, valid_cols)
+    valid_rows, valid_cols = np.where((hc >= -0.5) & (hc <= 0.5))
 
-###########################################################################################################
-# This part has to speed up (Jin -> Pete)
-    for k in range(0, rasterHC.RasterXSize - 1):
-        for kk in range(0, rasterHC.RasterYSize - 1):
-            # Check if hc value is within a certain range
-            if hc[kk][k] < 0.5 and hc[kk][k] > -0.5:
-                wl = astronomic_tide_resynthesis(t, omega, pha[kk][k][:], amp[kk][k][:])
-    
-                # Calculate the mean low water, mean sea level, and mean high water
-                mlw[kk][k] = mean_low_water(wl)
-                msl[kk][k] = np.average(wl)
-                mhw[kk][k] = mean_high_water(wl)
+    print("Size of t:", t.shape)
+    print("Size of omega:", omega.shape)
+    print("Size of pha:", pha.shape)
+    print("Size of amp:", amp.shape)
+    print("Size of wl:",valid_rows.shape)
 
-#######################################################################################
-# ######here is the chatGPT suggestion (Please confirm it)
+    # Initialize mlw, msl, and mhw arrays with NoData values
+    mlw = np.full_like(hc, ndv, dtype=float)
+    msl = np.full_like(hc, ndv, dtype=float)
+    mhw = np.full_like(hc, ndv, dtype=float)
 
-# # Create a mask based on the condition on hc values
-# mask = (hc < 0.5) & (hc > -0.5)
+    # Calculate mlw, msl, and mhw only for valid hc values
+    for kk, k in zip(valid_rows, valid_cols):
+        wl = astronomic_tide_resynthesis(t, omega, pha[kk, k, :], amp[kk, k, :]) # each row and column needs to calculate time series of astnomic tides (this part is time consuming)
+        mlw[kk, k] = mean_low_water(wl)
+        msl[kk, k] = np.average(wl)
+        mhw[kk, k] = mean_high_water(wl)
 
-# # Calculate wl for the entire array
-# ## wl need background? If so, make an array list with 0 or np.nan (Jin's comment)
-# wl = astronomic_tide_resynthesis(t, omega, pha[mask], amp[mask])
-
-# ##############################################################
-# # Especially please check this part (Jin's comments) #
-
-# # Assign calculated values to mlw and mhw arrays using the mask
-# mlw[mask] = mean_low_water(wl)
-# mhw[mask]  = mean_high_water(wl)
-        
-###########################################################################################################                
-                
-        # Check for bogus values and replace with NoData
-        # This can happen on the edges based on the raster resolution
-        mlw[(mlw < -99) | (mlw > 99)] = ndv
-        msl[(msl < -99) | (msl > 99)] = ndv
-        mhw[(mhw < -99) | (mhw > 99)] = ndv
+    # Check for bogus values and replace with NoData
+    # This can happen on the edges based on the raster resolution
+    mlw[(mlw < -99) | (mlw > 99)] = ndv
+    msl[(msl < -99) | (msl > 99)] = ndv
+    mhw[(mhw < -99) | (mhw > 99)] = ndv
     
 
     # --- WRITE OUTPUTS ---

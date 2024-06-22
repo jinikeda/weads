0b8aabe5ab1f011df9d393a8e838e8b4117f4f91
Jin Ikeda <jikeda@users.noreply.github.com>
2024-06-21 19:31:55 -0500

    improved code readabilities

--- src_branch/hydrodynamics.py
+++ src_branch/hydrodynamics.py
@@ -16,40 +16,71 @@ print("\n"); print("LAUNCH: Launching script!\n");
 #--- Read data as scatter points ---
 print("   Processing scatter points...\n")
 myFile=open("filteredData.pts","r")
-myLines=myFile.readlines(); numPoints=len(myLines);
-pts=np.ones((numPoints,11),dtype=float); myFile.seek(0);
+myLines=myFile.readlines();
+numPoints=len(myLines);
+pts=np.ones((numPoints,11),dtype=float);
+myFile.seek(0);
 for j in tqdm(range(numPoints)):
-    myLine=myFile.readline(); myRow=myLine.split();
-    pts[j][0]=int(myRow[0]); pts[j][1]=int(myRow[1]);
-    pts[j][2]=float(myRow[2]); pts[j][3]=float(myRow[3]);
-    pts[j][4]=float(myRow[4]); pts[j][5]=float(myRow[5]);
-    pts[j][6]=float(myRow[6]); pts[j][7]=float(myRow[7]);
-    pts[j][8]=float(myRow[8]); pts[j][9]=float(myRow[9]);
+    myLine=myFile.readline();
+    myRow=myLine.split();
+    pts[j][0]=int(myRow[0]);
+    pts[j][1]=int(myRow[1]);
+    pts[j][2]=float(myRow[2]);
+    pts[j][3]=float(myRow[3]);
+    pts[j][4]=float(myRow[4]);
+    pts[j][5]=float(myRow[5]);
+    pts[j][6]=float(myRow[6]);
+    pts[j][7]=float(myRow[7]);
+    pts[j][8]=float(myRow[8]);
+    pts[j][9]=float(myRow[9]);
     pts[j][10]=float(myRow[10])
 myFile.close()
 
-#--- Read harmomics correspondent with scatter ---
+#--- Read harWmomics correspondent with scatter ---
 print("\n"); print("   Processing harmonics...\n");
 myFile1=open("filteredHarmAmp.pts","r")
 myFile2=open("filteredHarmPha.pts","r")
 myFile3=open("harmonics.freq","r")
-myLine=myFile1.readline(); myRow=myLine.split(); numHarm=len(myRow); myFile1.seek(0);
-harmAMP=np.ones((numPoints,numHarm),dtype=float); harmPHA=np.ones((numPoints,numHarm),dtype=float);
+myLine=myFile1.readline();
+myRow=myLine.split();
+numHarm=len(myRow);
+myFile1.seek(0);
+harmAMP=np.ones((numPoints,numHarm),dtype=float);
+harmPHA=np.ones((numPoints,numHarm),dtype=float);
 freq=np.ones((numHarm,1),dtype=float)
 for j in range(numHarm):
-    myLine3=myFile3.readline(); myRow3=myLine3.split(); freq[j][0]=float(myRow3[0]);
+    myLine3=myFile3.readline();
+    myRow3=myLine3.split();
+    freq[j][0]=float(myRow3[0]);
 for j in tqdm(range(numPoints)):
-    myLine1=myFile1.readline(); myLine2=myFile2.readline();
-    myRow1=myLine1.split(); myRow2=myLine2.split();
+    myLine1=myFile1.readline();
+    myLine2=myFile2.readline();
+    myRow1=myLine1.split();
+    myRow2=myLine2.split();
     for jj in range(numHarm):
-        harmAMP[j][jj]=float(myRow1[jj]); harmPHA[j][jj]=float(myRow2[jj]); harmPHA[j][jj]=math.radians(harmPHA[j][jj]);
-myFile1.close(); myFile2.close(); myFile3.close();
+        harmAMP[j][jj]=float(myRow1[jj]);
+        harmPHA[j][jj]=float(myRow2[jj]);
+        harmPHA[j][jj]=math.radians(harmPHA[j][jj]);
+myFile1.close();
+myFile2.close();
+myFile3.close();
 
 #--- Calculate tidal datums for fully wetted zones ---
 print("\n"); print("   Calculating tidal datums for fully wetted zones...\n");
-T=30.0; dt=3600.0; N=int((86400*T/dt)+1); t=dt*np.ones((N,1),dtype=float); t=np.cumsum(t,axis=0);
-T2=2.0*T; D=24.0*3600.0/dt; D2=D/2.0; wl2=np.ones((int(D2),1),dtype=float); wl2L=np.ones((int(T2),1),dtype=float); wl2H=np.ones((int(T2),1),dtype=float);
-mlw=np.ones((numPoints,1),dtype=float); mhw=np.ones((numPoints,1),dtype=float); msl=np.ones((numPoints,1),dtype=float);
+T=30.0;
+dt=3600.0;
+N=int((86400*T/dt)+1);
+t=dt*np.ones((N,1),dtype=float);
+t=np.cumsum(t,axis=0);
+T2=2.0*T;
+D=24.0*3600.0/dt;
+D2=D/2.0;
+wl2=np.ones((int(D2),1),dtype=float);
+wl2L=np.ones((int(T2),1),dtype=float);
+wl2H=np.ones((int(T2),1),dtype=float);
+mlw=np.ones((numPoints,1),dtype=float);
+mhw=np.ones((numPoints,1),dtype=float);
+msl=np.ones((numPoints,1),dtype=float);
 for j in tqdm(range(numPoints)):
     if pts[j][8]>0.0:
         #--- Tidal resynthesis ---
@@ -60,20 +91,32 @@ for j in tqdm(range(numPoints)):
         for k in range(int(T2)):
             for kk in range(int(D2)):
                 wl2[kk][0]=wl[int(D2)*k+kk+1][0]
-            wl2L[k][0]=min(wl2); wl2H[k][0]=max(wl2);
-        msl[j][0]=np.mean(wl); mlw[j][0]=np.mean(wl2L); mhw[j][0]=np.mean(wl2H);
+            wl2L[k][0]=min(wl2);
+            wl2H[k][0]=max(wl2);
+        msl[j][0]=np.mean(wl);
+        mlw[j][0]=np.mean(wl2L);
+        mhw[j][0]=np.mean(wl2H);
     else:
-        msl[j][0]=-99999.0; mlw[j][0]=-99999.0; mhw[j][0]=-99999.0;
+        msl[j][0]=-99999.0;
+        mlw[j][0]=-99999.0;
+        mhw[j][0]=-99999.0;
 
 myOut=open("filteredTidalDatums.pts","w")
 for j in range(numPoints):
-    myOut.write(str(int(pts[j][0]))+" "); myOut.write(str(int(pts[j][1]))+" ");
-    myOut.write(str(pts[j][2])+" "); myOut.write(str(pts[j][3])+" ");
-    myOut.write(str(pts[j][4])+" "); myOut.write(str(pts[j][5])+" ");
-    myOut.write(str(pts[j][6])+" "); myOut.write(str(pts[j][7])+" ");
-    myOut.write(str(pts[j][8])+" "); myOut.write(str(pts[j][9])+" ");
-    myOut.write(str(pts[j][10])+" "); myOut.write(str(mlw[j][0])+" ");
-    myOut.write(str(msl[j][0])+" "); myOut.write(str(mhw[j][0])+"\n");
+    myOut.write(str(int(pts[j][0]))+" ");
+    myOut.write(str(int(pts[j][1]))+" ");
+    myOut.write(str(pts[j][2])+" ");
+    myOut.write(str(pts[j][3])+" ");
+    myOut.write(str(pts[j][4])+" ");
+    myOut.write(str(pts[j][5])+" ");
+    myOut.write(str(pts[j][6])+" ");
+    myOut.write(str(pts[j][7])+" ");
+    myOut.write(str(pts[j][8])+" ");
+    myOut.write(str(pts[j][9])+" ");
+    myOut.write(str(pts[j][10])+" ");
+    myOut.write(str(mlw[j][0])+" ");
+    myOut.write(str(msl[j][0])+" ");
+    myOut.write(str(mhw[j][0])+"\n");
 myOut.close()
 
 #--- Exit script ---
--- src_branch/preprocessing.py
+++ src_branch/preprocessing.py
@@ -13,6 +13,12 @@ import time
 start=time.time()
 print("\n"); print("LAUNCH: Launching script!\n");
 
+########################################################################################################################
+#### Jin's note need to add shaple file to extract the domain.
+# inputShapeFile
+#
+# June 20, 2024
+
 #--- Coordinate conversions (GEO-CPP) ---
 print("   Processing coordinate conversion...\n")
 re=6378206.4; lat=30.0;
@@ -23,15 +29,24 @@ ky=math.pi*re/180.0; ky=ky**1.0;
 print("   Processing bounding box...\n")
 bbox=[None]*4
 myFile=open("ROI.pts","r")
-myLine=myFile.readline(); myRow=myLine.split(); bbox[0]=float(myRow[0]); bbox[1]=float(myRow[1]);
-myLine=myFile.readline(); myRow=myLine.split(); bbox[2]=float(myRow[0]); bbox[3]=float(myRow[1]);
+myLine=myFile.readline();
+myRow=myLine.split();
+bbox[0]=float(myRow[0]);
+bbox[1]=float(myRow[1]);
+myLine=myFile.readline();
+myRow=myLine.split();
+bbox[2]=float(myRow[0]);
+bbox[3]=float(myRow[1]);
 myFile.close()
 
-#--- Read mesh ---
+########################################################################################################################
+#--- Read mesh (inputMeshFile) ---
 print("   Processing mesh...\n")
 myFile=open("fort.14","r")
 myLine=myFile.readline()
-myLine=myFile.readline(); myRow=myLine.split(); numNodes=int(myRow[1]);
+myLine=myFile.readline();
+myRow=myLine.split();
+numNodes=int(myRow[1]);
 nn=np.ones((numNodes,1),dtype=int);
 nx=np.ones((numNodes,1),dtype=float);
 ny=np.ones((numNodes,1),dtype=float);
@@ -39,51 +54,81 @@ nz=np.ones((numNodes,1),dtype=float);
 nxcpp=np.ones((numNodes,1),dtype=float);
 nycpp=np.ones((numNodes,1),dtype=float);
 for j in tqdm(range(numNodes)):
-    myLine=myFile.readline(); myRow=myLine.split();
-    N=int(myRow[0]); X=float(myRow[1]); Y=float(myRow[2]); Z=-float(myRow[3]);
-    nn[j][0]=N; nx[j][0]=X; ny[j][0]=Y; nz[j][0]=Z;
-    nxcpp[j][0]=X*kx; nycpp[j][0]=Y*ky;
+    myLine=myFile.readline();
+    myRow=myLine.split();
+    N=int(myRow[0]);
+    X=float(myRow[1]);
+    Y=float(myRow[2]);
+    Z=-float(myRow[3]);
+    nn[j][0]=N;
+    nx[j][0]=X;
+    ny[j][0]=Y;
+    nz[j][0]=Z;
+    nxcpp[j][0]=X*kx;
+    nycpp[j][0]=Y*ky;
 myFile.close()
 
-#--- Read attributes ---
+#--- Read attributes (inputMeshFile) ---
 print("\n"); print("   Processing attributes...\n");
 myFile=open("fort.13","r")
-myLine=myFile.readline()
-myLine=myFile.readline()
-myLine=myFile.readline(); myRow=myLine.split(); numAttributes=int(myRow[0]);
+# myLine=myFile.readline()
+# myLine=myFile.readline()
+myLine=myFile.readline();
+myRow=myLine.split();
+numAttributes=int(myRow[0]);
 for j in range(numAttributes):
-    myLine=myFile.readline(); myRow=myLine.split();
+    myLine=myFile.readline();
+    myRow=myLine.split();
     if myRow[0]=="mannings_n_at_sea_floor":
         myLine=myFile.readline()
         myLine=myFile.readline()
-        myLine=myFile.readline(); myRow=myLine.split(); mann0=float(myRow[0]);
+        myLine=myFile.readline();
+        myRow=myLine.split();
+        mann0=float(myRow[0]);
     else:
         myLine=myFile.readline()
         myLine=myFile.readline()
         myLine=myFile.readline()
-mann=np.ones((numNodes,1),dtype=float); mann=mann*mann0;
+mann=np.ones((numNodes,1),dtype=float);
+mann=mann*mann0; #(Jin's comments why?)
 for j in tqdm(range(numAttributes)):
-    myLine=myFile.readline(); myRow=myLine.split();
+    myLine=myFile.readline();
+    myRow=myLine.split();
     if myRow[0]=="mannings_n_at_sea_floor":
-        myLine=myFile.readline(); myRow=myLine.split(); numMann=int(myRow[0]);
+        myLine=myFile.readline();
+        myRow=myLine.split();
+        numMann=int(myRow[0]);
         for jj in range(numMann):
-            myLine=myFile.readline(); myRow=myLine.split(); NN=int(myRow[0]); mann[NN-1][0]=float(myRow[1]);
+            myLine=myFile.readline();
+            myRow=myLine.split();
+            NN=int(myRow[0]);
+            mann[NN-1][0]=float(myRow[1]);
     else:
-        myLine=myFile.readline(); myRow=myLine.split(); numAttr=int(myRow[0]);
+        myLine=myFile.readline();
+        myRow=myLine.split();
+        numAttr=int(myRow[0]);
         for jj in range(numAttr):
             myLine=myFile.readline()
 myFile.close()
 
-#--- Read everdried ---
-print("\n"); print("   Processing everdried...\n");
-myFile=open("everdried.63","r")
-myLine=myFile.readline()
-myLine=myFile.readline()
-myLine=myFile.readline()
-ed=np.ones((numNodes,1),dtype=float)
-for j in range(numNodes):
-    myLine=myFile.readline(); myRow=myLine.split(); ed[j][0]=float(myRow[1]);
-myFile.close()
+
+########################################################################################################################
+# This code have to replace to inundationTime.63
+# Short Comments
+# Jin June 20, 2024
+# Also, need to consider to read netcdf file. insall netCDF4, xarray?
+########################################################################################################################
+
+# #--- Read everdried ---
+# print("\n"); print("   Processing everdried...\n");
+# myFile=open("everdried.63","r")
+# myLine=myFile.readline()
+# myLine=myFile.readline()
+# myLine=myFile.readline()
+# ed=np.ones((numNodes,1),dtype=float)
+# for j in range(numNodes):
+#     myLine=myFile.readline(); myRow=myLine.split(); ed[j][0]=float(myRow[1]);
+# myFile.close()
 
 #--- Read inundationtime ---
 print("   Processing inundationtime...\n")
@@ -93,57 +138,80 @@ myLine=myFile.readline()
 myLine=myFile.readline()
 it=np.ones((numNodes,1),dtype=float)
 for j in range(numNodes):
-    myLine=myFile.readline(); myRow=myLine.split(); it[j][0]=float(myRow[1]);
+    myLine=myFile.readline();
+    myRow=myLine.split();
+    it[j][0]=float(myRow[1]);
 myFile.close()
 
-#--- Read maxinundepth ---
-print("   Processing maxinundepth...\n")
-myFile=open("maxinundepth.63","r")
-myLine=myFile.readline()
-myLine=myFile.readline()
-myLine=myFile.readline()
-mi=np.ones((numNodes,1),dtype=float)
-for j in range(numNodes):
-    myLine=myFile.readline(); myRow=myLine.split(); mi[j][0]=float(myRow[1]);
-myFile.close()
+########################################################################################################################
+# #--- Read maxinundepth ---
+# print("   Processing maxinundepth...\n")
+# myFile=open("maxinundepth.63","r")
+# myLine=myFile.readline()
+# myLine=myFile.readline()
+# myLine=myFile.readline()
+# mi=np.ones((numNodes,1),dtype=float)
+# for j in range(numNodes):
+#     myLine=myFile.readline(); myRow=myLine.split(); mi[j][0]=float(myRow[1]);
+# myFile.close()
 
-# --- Read harmonics ---
+# --- Read harmonics (inputHarmonicsFile) ---
 print("   Processing harmonics...\n")
-myFile=open("fort.53","r"); myOut=open("harmonics.freq","w");
-myLine=myFile.readline(); myRow=myLine.split(); numHarm=int(myRow[0]);
-harmAMP=np.ones((numNodes,numHarm),dtype=float); harmPHA=np.ones((numNodes,numHarm),dtype=float);
+myFile=open("fort.53","r");
+myOut=open("harmonics.freq","w");
+myLine=myFile.readline();
+myRow=myLine.split();
+numHarm=int(myRow[0]);
+harmAMP=np.ones((numNodes,numHarm),dtype=float);
+harmPHA=np.ones((numNodes,numHarm),dtype=float);
 for j in range(numHarm):
-    myLine=myFile.readline(); myRow=myLine.split(); myOut.write(str(myRow[0])+"\n");
+    myLine=myFile.readline();
+    myRow=myLine.split();
+    myOut.write(str(myRow[0])+"\n");
 myLine=myFile.readline()
 for j in tqdm(range(numNodes)):
     myLine=myFile.readline()
     for jj in range(numHarm):
-        myLine=myFile.readline(); myRow=myLine.split();
-        harmAMP[j][jj]=float(myRow[0]); harmPHA[j][jj]=float(myRow[1]);
+        myLine=myFile.readline();
+        myRow=myLine.split();
+        harmAMP[j][jj]=float(myRow[0]);
+        harmPHA[j][jj]=float(myRow[1]);
 myFile.close()
 
 #--- Filter for bounding box ---
 print("\n"); print("   Filtering data for bounding box...\n");
 myOut1=open("filteredData.pts","w")
-myOut2=open("filteredHarmAmp.pts","w"); myOut3=open("filteredHarmPha.pts","w");
+myOut2=open("filteredHarmAmp.pts","w");
+myOut3=open("filteredHarmPha.pts","w");
 cnt=0
 for j in tqdm(range(numNodes)):
     if nx[j][0]>bbox[0] and nx[j][0]<bbox[2]:
         if ny[j][0]>bbox[1] and ny[j][0]<bbox[3]:
             cnt=cnt+1
-            myOut1.write(str(cnt)+" "); myOut1.write(str(j+1)+" ");
-            myOut1.write(str(nx[j][0])+" "); myOut1.write(str(ny[j][0])+" ");
-            myOut1.write(str(nxcpp[j][0])+" "); myOut1.write(str(nycpp[j][0])+" ");
-            myOut1.write(str(nz[j][0])+" "); myOut1.write(str(mann[j][0])+" ");
-            myOut1.write(str(ed[j][0])+" "); myOut1.write(str(it[j][0])+" ");
-            myOut1.write(str(mi[j][0])+"\n")
+            myOut1.write(str(cnt)+" ");
+            myOut1.write(str(j+1)+" ");
+            myOut1.write(str(nx[j][0])+" ");
+            myOut1.write(str(ny[j][0])+" ");
+            myOut1.write(str(nxcpp[j][0])+" "); # not sure Jin June 20,2024
+            myOut1.write(str(nycpp[j][0])+" "); # not sure
+            myOut1.write(str(nz[j][0])+" ");
+            myOut1.write(str(mann[j][0])+" "); #manning?
+            myOut1.write(str(ed[j][0])+" "); # where is this variable?
+            myOut1.write(str(it[j][0])+" ");
+            myOut1.write(str(mi[j][0])+"\n") # where is this variable?
             for jj in range(numHarm-1):
-                myOut2.write(str(harmAMP[j][jj])+" "); myOut3.write(str(harmPHA[j][jj])+" ");
-            myOut2.write(str(harmAMP[j][jj+1])+"\n"); myOut3.write(str(harmPHA[j][jj+1])+"\n");
-myOut1.close(); myOut2.close(); myOut3.close();
+                myOut2.write(str(harmAMP[j][jj])+" ");
+                myOut3.write(str(harmPHA[j][jj])+" ");
+            myOut2.write(str(harmAMP[j][jj+1])+"\n");
+            myOut3.write(str(harmPHA[j][jj+1])+"\n");
+myOut1.close();
+myOut2.close();
+myOut3.close();
 
 #--- Exit script ---
-print("\n"); print("EXIT: Existing script!\n");
-end=time.time(); print ("Time elapsed (seconds):",end-start);
+print("\n");
+print("EXIT: Existing script!\n");
+end=time.time();
+print ("Time elapsed (seconds):",end-start);
 
 
